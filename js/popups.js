272190cd2bd131c6e50e6aaccfade03963bd852c
Josue Ysai Martinez Morales <zunecks@gmail.com>
2020-12-31 15:35:01 -0600

    Popups

--- js/popups.js
+++ js/popups.js
@@ -13,3 +13,23 @@ btnCerrarPopup.addEventListener('click', function(e){
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 });
+
+
+var btnAbrirPopup2 = document.querySelectorAll('.btn-abrir-popup2'),
+	overlay2 = document.getElementById('overlay2'),
+	popup2 = document.getElementById('popup2'),
+	btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
+
+	for (var i = 0; i < btnAbrirPopup2.length; i++){
+
+btnAbrirPopup2[i].addEventListener('click', function(){
+	overlay2.classList.add('active');
+	popup2.classList.add('active');
+});
+
+btnCerrarPopup2.addEventListener('click', function(e2){
+	e2.preventDefault();
+	overlay2.classList.remove('active');
+	popup2.classList.remove('active');
+});
+	}
