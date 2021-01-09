8eca32b62f5844a2e28a4070febdff15525e4618
Nanye13 <52982963+Nanye13@users.noreply.github.com>
2021-01-09 02:36:23 -0600

    Add files via upload

--- modelos/devolucion.java
+++ modelos/devolucion.java
@@ -1,5 +1,35 @@
 package modelos;
 
 public class devolucion {
+    private int id_devolucion;
+    private int proveedor;
+    private int zapato;
+    
+    public devolucion( int id_devolucion, int proveedor, int zapato){
+        this.id_devolucion= id_devolucion;
+        this.proveedor= proveedor;
+        this.zapato= zapato;
+    }
+    public devolucion(){
+
+    }
+    public int getId_devolucion(){
+        return id_devolucion;
+    }
+    public void setId_devolucion( int id_devolucion) {
+        this.id_devolucion= id_devolucion;
+    }
+    public int getProveedor(){
+        return proveedor;
+    }
+    public void setProveedor( int proveedor){
+        this.proveedor= proveedor;
+    }
+    public int getZapato(){
+        return zapato;
+    }
+    public void setZapato( int zapato){
+        this.zapato= zapato;
+    }
     
 }
--- modelos/venta.java
+++ modelos/venta.java
@@ -1,5 +1,75 @@
 package modelos;
 
+import java.text.DecimalFormat;
+import java.util.Date;
+
 public class venta {
+    private Date fecha;
+    private DecimalFormat total;
+    private DecimalFormat recibido;
+    private DecimalFormat cambio;
+    private int modelo;
+    private int id_usuario;
+
+    public venta(Date fecha, DecimalFormat total, DecimalFormat recibido, DecimalFormat cambio, int modelo,
+            int id_usuario) {
+        this.setFecha(fecha);
+        this.setTotal(total);
+        this.setRecibido(recibido);
+        this.setCambio(cambio);
+        this.setModelo(modelo);
+        this.setId_usuario(id_usuario);
+    }
+    public venta(){
+        
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public DecimalFormat getTotal() {
+        return total;
+    }
+
+    public void setTotal(DecimalFormat total) {
+        this.total = total;
+    }
+
+    public DecimalFormat getRecibido() {
+        return recibido;
+    }
+
+    public void setRecibido(DecimalFormat recibido) {
+        this.recibido = recibido;
+    }
+
+    public DecimalFormat getCambio() {
+        return cambio;
+    }
+
+    public void setCambio(DecimalFormat cambio) {
+        this.cambio = cambio;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
     
 }
