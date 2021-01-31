aeda3494dfd03b804220d12cfb20a628f9089ce9
Alejandro Platas <alejandro.platas.02@gmail.com>
2021-01-30 19:42:53 -0600

    paginacion

--- principal-admin.php
+++ principal-admin.php
@@ -26,10 +26,15 @@
     <link rel="shortcut icon" type="image/png" href="img/icon.png" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <!-- Our Custom CSS -->
+<link rel="stylesheet" href="style2.css">
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 </head>
 
 <body>
-    <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+
+    <div class="page-wrapper default-theme sidebar-bg bg1 toggled wrapper">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
                 aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -81,13 +86,13 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="Inventario-Administrador.php">
+                                    <a href="Inventario-Administrador.php?pagina=1">
                                         <i class="fa fa-warehouse"></i>
                                         <span class="menu-text">Inventario</span>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="reportes.php">
+                                    <a href="reportes.php?pagina=1">
                                         <i class="fa fa-chart-line"></i>
                                         <span class="menu-text">Reportes</span>
                                     </a>
@@ -99,9 +104,9 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="devoluciones.html">
-                                        <i class="fa fa-sync-alt"></i>
-                                        <span class="menu-text">Devoluciones</span>
+                                    <a href="apartado.php">
+                                    <i class="fa fa-cart-plus"></i>
+                                        <span class="menu-text">Apartados</span>
                                     </a>
                                 </li>
                                 <li>
@@ -111,7 +116,7 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="Proveedor-Administrador.php">
+                                    <a href="Proveedor-Administrador.php?pagina=1">
                                         <i class="fa fa-truck"></i>
                                         <span class="menu-text">Proveedores</span>
                                     </a>
@@ -171,13 +176,14 @@
                     </div>
                 </div>
             </div>
+<!---------->            
 
             <div id="overlay" class="overlay"></div>
             <div class="container p-5">
                 <div class="row">
                     <div class="form-group col-md-3 ">
                         <div class="article">
-                            <a href="Inventario-Administrador.php">
+                            <a href="Inventario-Administrador.php?pagina=1">
                                 <img src="img/cajitas.svg" width="200" height="150" alt="">
                                 <p>Inventario</p>
                             </a>
@@ -193,7 +199,7 @@
                     </div>
                     <div class="form-group col-md-3">
                         <div class="article">
-                            <a href="reportes.php">
+                            <a href="reportes.php?pagina=1">
                                 <img src="img/document.svg" width="200" height="150" alt="">
                                 <p>Reportes</p>
                             </a>
@@ -210,15 +216,15 @@
 
                     <div class="form-group col-md-3">
                         <div class="article">
-                            <a href="devoluciones.html">
+                            <a href="apartado.php">
                                 <img src="img/broked.svg" width="200" height="150" alt="">
-                                <p>Devoluciones</p>
+                                <p>Apartado</p>
                             </a>
                         </div>
                     </div>
                     <div class="form-group col-md-3">
                         <div class="article">
-                            <a href="Proveedor-Administrador.php">
+                            <a href="Proveedor-Administrador.php?pagina=1">
                                 <img src="img/provider.svg" width="200" height="150" alt="">
                                 <p>Proveedores</p>
                             </a>
@@ -256,6 +262,15 @@
         integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
         </script>
     <script src="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+     <!-- jQuery CDN - Slim version (=without AJAX) -->
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
 
     <!-- using local scripts -->
     <!-- <script src="../node_modules/jquery/dist/jquery.min.js"></script>
